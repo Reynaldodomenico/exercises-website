d9787899ac8a6a44f4d3c76d01cb7ae8688c7486
Reynaldo Domenico <reynaldodomenico@yahoo.com>
2022-06-15 12:21:38 +0200

    adding exercises detail and video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,20 @@
-import React, {useState} from "react";
-import {Box} from '@mui/material';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
-import Exercises from "../components/Exercises";
-import HeroBanner from "../components/HeroBanner";
-import SearchExercises from "../components/SearchExercises";
-import HorizontalScrollBar from "../components/HorizontalScrollBar";
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
-  const [bodyPart, setBodyPart] = useState([]);
   const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
 
   return (
-  <Box>
-    <HeroBanner/>
-    <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
-    <Exercises exercises={exercises} setExercises={setExercises} bodyPart={bodyPart}/>
-  </Box>
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
   );
 };
 
